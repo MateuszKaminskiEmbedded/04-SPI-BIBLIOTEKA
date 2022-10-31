f321a3b704990177ed3729041aaa79c6017531f9
M_Kamiński <104322532+MateuszKaminskiEmbedded@users.noreply.github.com>
2022-10-31 22:47:30 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <xc.h>
+#include "DeviceConfig.h"
+#include "SPI.h"
+
+void main(void){
+	// Ustaw porty jako cyfrowe
+    ANSELA = 0;
+    ANSELB = 0;
+    ANSELC = 0;
+    ANSELE = 0;
+    ANSELG = 0;
+    
+    // Set up PPS for SPI 1
+    TRISFbits.TRISF1 = 1;
+	SDI1R = 0b1011;         // RF1 = SPI1 SDI
+
+    TRISBbits.TRISB7 = 0;   // RB7 = SPI1 CLK
+
+    TRISFbits.TRISF0 = 0;
+	SS1R = 0b1100;          // RF0 = SPI1 SS    // optional. Inaczej CS - Chip Select
+
+    TRISAbits.TRISA0 = 0;
+    RPA0R = 0b00011;        // RA0 = SPI1 SDO
+    
+	Set_MaxSpeed_Mode();
+    SPI1_init();
+	
+    while (1){
+
+    }
+}
